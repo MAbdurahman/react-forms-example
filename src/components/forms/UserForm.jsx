48be1120a9209b69573fa37752067aede8b575f6
MAbdurahman <mdbdrrhm@gmail.com>
2021-01-28 09:52:23 -0600

    End of chapter 6.2 and 6.3; completed

--- src/components/forms/UserForm.jsx
+++ src/components/forms/UserForm.jsx
@@ -3,14 +3,16 @@ import { Formik, Form, Field, ErrorMessage } from 'formik'
 import './UserForm.css';
 
 export default class UserForm extends Component {
+
    constructor(props) {
       super(props);
 
    }
 
+
    render() {
-      // const email_pattern = /^[A-Za-z\.\-_0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
       const email_pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+      const password_pattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/i;
 
       return (
          <div className='contact-form'>
@@ -25,27 +27,44 @@ export default class UserForm extends Component {
                   } else if (!email_pattern.test(values.email)) {
                      errors.email = 'Invalid email address!';
                   }
+
+                  if (!values.password) {
+                     errors.password = 'Password is required!';
+
+                  } else if (!password_pattern.test(values.password)) {
+                     errors.password = 'Password - minimum 8 characters with 1 uppercase, 1 lowercase, 1 number, and 1 special character!';
+
+                  }
+
                   return errors;
                }}
                onSubmit={(values, { setSubmitting }) => {
                   setTimeout(() => {
                      alert(JSON.stringify(values, null, 2));
                      setSubmitting(false);
-                  }, 400);
+                  }, 500);
                }}
             >
                {({ isSubmitting }) => (
                   <Form>
                      <div>
-                        <Field type='email' name='email' fullWidth />
+                        <Field
+                           type='email'
+                           name='email'
+                           placeholder='email address'
+                        />
                         <ErrorMessage name='email' component='span' />
                      </div>
                      <div>
-                        <Field type='password' name='password' fullWidth />
-                        <Field type='password' component='span' />
+                        <Field
+                           type='password'
+                           name='password'
+                           placeholder='password'
+                        />
+                        <ErrorMessage name='password' component='span' />
                      </div>
 
-                     <button type='submit' disabled={isSubmitting} fullWidth>
+                     <button type='submit' disabled={isSubmitting}>
                         Submit
                      </button>
 
@@ -53,7 +72,7 @@ export default class UserForm extends Component {
                )}
 
             </Formik>
-         </div>
+         </div >
       );
    };
 };
